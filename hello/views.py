fcd480e13d38abc818c8ea1671fdeeba93eedd17
Connor Bradley <cpbrad31@gmail.com>
2025-01-01 22:11:06 -0500

    Change config vars

--- hello/views.py
+++ hello/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 import requests
 from django.http import HttpResponse
 from .models import Greeting
+import os
+from django.http import HttpResponse
+
 
 # Create your views here.
 
 
 def index(request):
-    r = requests.get('https://httpbin.org/status/418', timeout=10)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES', 3))
+    return HttpResponse('Hello! ' * times)
 
 
 def db(request):
